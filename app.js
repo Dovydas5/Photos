aadbe6b7391b2984bd8e79818338a50dcb98c827
Dovydasp <59613922+Dovydasp@users.noreply.github.com>
2020-05-18 12:58:00 +0300

    search query

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let searchValue;
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
-    "https://api.pexels.com/v1/curated?per_page=1",
+    "https://api.pexels.com/v1/curated?per_page=15&page=1",
     {
       method: "GET",
       headers: {
@@ -17,7 +17,6 @@ async function curatedPhotos() {
   );
   const data = await dataFetch.json();
   data.photos.forEach(photo => {
-    console.log(photo);
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
     galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
@@ -25,4 +24,25 @@ async function curatedPhotos() {
         gallery.appendChild(galleryImg)
   });
 }
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth
+      }
+    }
+  );
+  const data = await dataFetch.json();
+  data.photos.forEach(photo => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
+        <p>${photo.photographer}</p>`;
+        gallery.appendChild(galleryImg)
+  });
+}
+
 curatedPhotos();
